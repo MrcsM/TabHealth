da34092e7d70313ddaf9d85bf207c05584d82307
Marcus <marcus.moquin@hotmail.com>
2021-07-08 03:55:16 -0400

    ok i added absorption yippee

--- src/me/baneofsmite/tabhealth/Main.java
+++ src/me/baneofsmite/tabhealth/Main.java
@@ -1,45 +1,50 @@
 package me.baneofsmite.tabhealth;
 
+import net.minecraft.server.v1_8_R3.EntityPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.*;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
-public class Main extends JavaPlugin { //Leon Is Awesome, Play on Arctic. Also Bane Is Cool, Play on 2 Donkeys
+import java.util.Iterator;
+
+public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         Scoreboard board = manager.getMainScoreboard();
+        Objective below;
+        if (board.getObjective("HealthBelow") == null) {
+            below = board.registerNewObjective("HealthBelow", "dummy");
+        } else {
+            below = board.getObjective("HealthBelow");
+        }
 
-        Objective name;
         Objective tab;
-
-        if (board.getObjective("HealthNamePL") == null) {
-            name = board.registerNewObjective("HealthNamePL", "dummy");
+        if (board.getObjective("HealthTab") == null) {
+            tab = board.registerNewObjective("HealthTab", "dummy");
         } else {
-            name = board.getObjective("HealthNamePL");
+            tab = board.getObjective("HealthTab");
         }
 
-        if (board.getObjective("HealthTabPL") == null) {
-            tab = board.registerNewObjective("HealthTabPL", "dummy");
-        } else {
-            tab = board.getObjective("HealthTabPL");
-        }
-
-        name.setDisplaySlot(DisplaySlot.BELOW_NAME);
-        name.setDisplayName(ChatColor.RED + "❤");
-        
+        below.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        below.setDisplayName(ChatColor.AQUA + "❤");
         tab.setDisplaySlot(DisplaySlot.PLAYER_LIST);
-
         Bukkit.getScheduler().runTaskTimer(this, () -> {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                int health = (int) Math.floor((player.getHealth() / 20) * 100);
-                
-                name.getScore(player.getName()).setScore(health);
+            Iterator var3 = Bukkit.getOnlinePlayers().iterator();
+
+            while (var3.hasNext()) {
+                CraftPlayer player = (CraftPlayer) var3.next();
+                EntityPlayer entityPlayer = player.getHandle();
+                int health = (int)Math.floor((player.getHealth() / 20.0D * 100.0D) + entityPlayer.getAbsorptionHearts() * 5);
+                below.getScore(player.getName()).setScore(health);
                 tab.getScore(player.getName()).setScore(health);
             }
-        }, 1L, 1L);
+        }, 0L, 10L);
     }
 }
